0b0c3b70a4ea8a24aeed040078fb2330807e13d2
alexbowes123 <alexbowes1@hotmail.com>
2022-02-12 19:00:37 +0000

    add user route and post test

--- c18727635_fyp/api/index.js
+++ c18727635_fyp/api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -12,9 +13,15 @@ mongoose.connect(
     console.log(err);
 });
 
+// To allow for JSON to be used
+app.use(express.json());
+
 app.get("/api/test", () =>{
     console.log("test is successful!");
 })
+
+//when going to a /user endpoint, this will bring to user.js
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
    console.log("Backend server running!") 
