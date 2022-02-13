cfeadb460a768bedd2f5e7c7a853f5813f44475d
alexbowes123 <alexbowes1@hotmail.com>
2022-02-13 15:58:51 +0000

    create authjs for test register user + added user to db

--- c18727635_fyp/api/index.js
+++ c18727635_fyp/api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -13,15 +14,19 @@ mongoose.connect(
     console.log(err);
 });
 
-// To allow for JSON to be used
-app.use(express.json());
+
 
 app.get("/api/test", () =>{
     console.log("test is successful!");
 })
 
+// To allow for JSON to be used
+app.use(express.json());
+
 //when going to a /user endpoint, this will bring to user.js
 app.use("/api/users", userRoute);
+
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
    console.log("Backend server running!") 
--- c18727635_fyp/api/routes/user.js
+++ c18727635_fyp/api/routes/user.js
@@ -1,14 +1,6 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req,res)=>{
-    res.send("user test is successful");
-});
 
-router.post("/userposttest", (req,res)=>{
-    const username = req.body.username;
-    console.log(username);
-
-});
 
 
 module.exports = router
