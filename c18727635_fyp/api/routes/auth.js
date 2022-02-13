49a16fd60f3c5349dd6e6cd71c739f6526a67970
alexbowes123 <alexbowes1@hotmail.com>
2022-02-13 18:41:08 +0000

    add login func and crypto-js for password

--- c18727635_fyp/api/routes/auth.js
+++ c18727635_fyp/api/routes/auth.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const { rawListeners } = require("../models/User");
 
 // REGISTRATION
 router.post("/register", async (req,res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
     try{
         const savedUser = await newUser.save();
@@ -17,5 +19,27 @@ router.post("/register", async (req,res)=>{
     } 
 });
 
+router.post("/login", async (req,res)=>{
+    try{
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json("Wrong user");
 
-module.exports = router
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        
+        OriginalPassword !== req.body.password && 
+            res.status(401).json("Wrong password");
+
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others);
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
