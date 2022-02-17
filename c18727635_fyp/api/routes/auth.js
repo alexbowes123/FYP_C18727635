6745240464c1b48cd94f0e21bf614ab35f5b6695
alexbowes123 <alexbowes1@hotmail.com>
2022-02-17 15:19:13 +0000

    add JWT, admin token, get users and some stats

--- c18727635_fyp/api/routes/auth.js
+++ c18727635_fyp/api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 const { rawListeners } = require("../models/User");
 
 // REGISTRATION
@@ -18,7 +19,7 @@ router.post("/register", async (req,res)=>{
         res.status(500).json(err);
     } 
 });
-
+//LOGIN SECURELY
 router.post("/login", async (req,res)=>{
     try{
         const user = await User.findOne({username: req.body.username});
@@ -33,9 +34,17 @@ router.post("/login", async (req,res)=>{
         OriginalPassword !== req.body.password && 
             res.status(401).json("Wrong password");
 
+            const accessToken = jwt.sign({
+                id:user._id,
+                isAdmin: user.isAdmin,
+            },
+            process.env.JWT_SEC,
+            {expiresIn:"3d"}
+            );
+
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     } catch(err){
         res.status(500).json(err);
     }
--- c18727635_fyp/api/routes/user.js
+++ c18727635_fyp/api/routes/user.js
@@ -1,6 +1,90 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
+
 const router = require("express").Router();
 
 
+router.put("/:id", verifyTokenAndAuthorization, async (req,res)=>{
+    if(req.body.password) {
+        req.body.password = CryptoJS.AES.decrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+    }
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        },
+        {new:true}
+    );
+    res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("Deleted user")
+    } catch (error){
+        res.status(500).json(error);
+    }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc;
+
+        res.status(200).json(others)
+    } catch (error){
+        res.status(500).json(error);
+    }
+    
+});
+
+// Get every user
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new
+    try {
+        // if there is a query = new, return 5 latests users. if not, return all users
+        // sort by id descending
+        const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find()
+        res.status(200).json(users)
+    } catch (error){
+        res.status(500).json(error);
+    }
+    
+});
+
+//Get user stats
+//Get how many users were made in each month (1 = jan, 2 = feb)
+router.get("/stats", verifyTokenAndAdmin, async (req, res)=>{
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try{
+
+        const data = await User.aggregate([
+            {$match: {createdAt: {$gte: lastYear}}},
+            {
+                $project:{
+                    month: { $month: "$createdAt" }
+                },
+            },
+            {
+                $group:{
+                    _id: "$month",
+                    total:{$sum: 1},
+                }
+            }
+        ]);
+        res.status(200).json(data);
+    }catch(error) {
+        res.status(500).json(err);
+    }
+})
 module.exports = router
