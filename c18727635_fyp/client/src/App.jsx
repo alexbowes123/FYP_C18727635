619e8e7ea91a1a33bd47e793bd3d9f778f4102a0
alexbowes123 <alexbowes1@hotmail.com>
2022-02-21 12:45:42 +0000

    add react router to client to change page at url

--- c18727635_fyp/client/src/App.jsx
+++ c18727635_fyp/client/src/App.jsx
@@ -1,8 +1,30 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 const App = () => {
-    return <ProductList/>;
+    return (
+
+        // Using react router dom
+        <Router>
+            <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/products' element={<ProductList/>} />
+              
+                {/* </Route> */}
+                {/* <Route path = '/products'>
+                    <ProductList/>
+                </Route> */}
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
