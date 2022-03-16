7909dfe5104657d8994cafeed6110621c20c486b
alexbowes123 <alexbowes1@hotmail.com>
2022-03-16 15:21:02 +0000

    add refresh functionality, logout and trigger axios on registration

--- c18727635_fyp/api/routes/auth.js
+++ c18727635_fyp/api/routes/auth.js
@@ -3,6 +3,10 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 const { rawListeners } = require("../models/User");
+const { application } = require("express");
+const {verifyTokenAndAdmin, verifyToken} = require("./verifyToken");
+
+
 
 // REGISTRATION
 router.post("/register", async (req,res)=>{
@@ -29,6 +33,70 @@ router.post("/register", async (req,res)=>{
         res.status(500).json(err);
     } 
 });
+
+let refreshTokens = [];
+
+router.post("/refresh", async (req,res)=>{
+    //take the refresh token from user
+    const refreshToken = req.body.token
+
+    //send error if there is no token or invalid token
+    if(!refreshToken){
+        return res.status(401).json("You are not authenticated!");
+    } 
+
+    if(!refreshTokens.includes(refreshToken)){
+        return res.status(403).json("Refresh token is not in array")
+    }
+
+    //if everything is ok, create new refresh and access tokens
+    jwt.verify(refreshToken, process.env.JWT_REF, (err,user)=>{
+        err && console.log(err);
+        refreshTokens =refreshTokens.filter((token)=> token !== refreshToken);
+        
+        
+        console.log("about to generate new access and refresh");
+        const newAccessToken = generateAccessToken(user); //is user reaching here?
+        const newRefreshToken = generateRefreshToken(user);
+        console.log("refresh generation and array push!")
+        console.log("new access token with user passed is")
+        console.log(newAccessToken);
+        console.log("new refresh with user passed is");
+        console.log(newRefreshToken);
+
+        refreshTokens.push(newRefreshToken);
+
+        //new refrsh and access are reaching react
+        res.status(200).json({
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken
+        });
+    });
+
+    //if all is ok, create a new access token, refresh token and send to user
+})
+
+
+
+const generateAccessToken =(user)=>{
+    return jwt.sign({
+        id:user._id,
+        isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SEC,
+    {expiresIn:"40s"}
+    );
+} 
+
+const generateRefreshToken =(user)=>{
+    return jwt.sign({
+        id:user._id,
+        isAdmin: user.isAdmin,
+    },
+    process.env.JWT_REF
+    );
+}
+
 //LOGIN SECURELY
 router.post("/login", async (req,res)=>{
     try{
@@ -57,13 +125,9 @@ router.post("/login", async (req,res)=>{
         }  
         
         //when logging in successfully, make a 3 day token
-        const accessToken = jwt.sign({
-            id:user._id,
-            isAdmin: user.isAdmin,
-        },
-        process.env.JWT_SEC,
-        {expiresIn:"3d"}
-        );
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+        refreshTokens.push(refreshToken);
 
         const { password, ...others } = user._doc;
 
@@ -72,13 +136,23 @@ router.post("/login", async (req,res)=>{
 
         console.log(accessToken);
 
-        res.status(200).json({...others, accessToken});
+        res.status(200).json({...others, accessToken, refreshToken});
         console.log("user logged in!");
     } catch(err){
         console.log("potatoes");
         res.status(500).json(err);
     }
 });
+
+router.post("/logout", verifyToken, (req,res)=>{
+
+    console.log("refToken is")
+    const refreshToken = req.body.token;
+
+    console.log(refreshToken);
+    refreshTokens = refreshTokens.filter(token => token !== refreshToken);
+    res.status(200).json("logged out successfully")
+})
 
 module.exports = router;
 
